76d4e3bf352b85404c6c974176e9fb520f309cc1
Rajesh <geekyshows@gmail.com>
2023-06-19 14:31:32 +0530

    first commit

---
+++ blog/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
